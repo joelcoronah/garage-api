5eec368f8e8dd30adb8e4fb97ecb3788ea479a95
joelcoronah <joelcoronah@gmail.com>
2020-09-18 22:22:18 -0400

    :wrench: add new configuration files, environment files, db conection and some typing packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    const cors = {
+        origin: [
+            '*',
+        ],
+        methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        credentials: true,
+        allowedHeaders: [
+            'Accept',
+            'Content-Type',
+            'Authorization',
+            'token',
+        ],
+    };
+    app.enableCors(cors);
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    await app.listen(3000);
 }
 bootstrap();
